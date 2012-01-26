60346f7d70e3cf44ac165ec57a0c13fea851bff7
art <lekcjePHP@gmail.com>
2012-01-26 18:54:48 +0000

    odwaracanie stringa sie powiodlo

--- msin.py
+++ msin.py
@@ -1,7 +1,9 @@
 # dla 'ola' zwroci 'lo', dla 'kacper': 'epcak'
-def odwracanies(string):
+def odwracanie(string):
 	newString = string[0:-1]
-	newString.reverse();
-	return newString;
+	newString = list(newString)#zamiana na tablice
+	newString.reverse()
+	newString = ''.join(newString)#laczenie w stronga
+	return (newString)
 
-
+print(odwracanie("alicja"))
